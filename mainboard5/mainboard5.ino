717829c91328b8e75d0ba84a0bc737fd1b28f28b
取火的普罗米修斯的GitHub <43365415+Chaoschaos1024@users.noreply.github.com>
2025-12-17 22:18:21 +0800

    fix bug

--- mainboard5/mainboard5.ino
+++ mainboard5/mainboard5.ino
@@ -1,8 +1,8 @@
 /*
    @Author: 无序熵增
    @Date: 2025-07-24 17:59:36
-   @LastEditors: 无序熵增
-   @LastEditTime: 2025-11-03 16:04:14
+ * @LastEditors: 无序熵增
+ * @LastEditTime: 2025-12-17 22:14:01
    @Description:
 
    Copyright (c) 2025 by 无序熵增, All Rights Reserved.
@@ -33,7 +33,8 @@ unsigned int delay_time = 0;
 long long int loop_count_debug = 0;
 int allwaysdelay = 0;
 #endif
-void setup() {
+void setup()
+{
   io.begin();
 #if debug
   Serial1.println("-----------------------------------------------------------------------------------------------");
@@ -82,7 +83,8 @@ void setup() {
 #endif
 }
 
-void loop() {
+void loop()
+{
 #if debug
   long long int time_now = rp2040.getCycleCount64();
   Serial1.print("loop begin. loop count : ");
@@ -90,7 +92,7 @@ void loop() {
   Serial1.print("/");
   Serial1.println(loop_count_max);
 #endif
-  kb.routine(mouse_x, mouse_y);  //
+  kb.routine(mouse_x, mouse_y); //
   mouse_x = mouse_x + kb.mouse_x_ * kb.stick_speed - 128 * kb.stick_speed;
   mouse_y = mouse_y + kb.mouse_y_ * kb.stick_speed - 128 * kb.stick_speed;
   kb.mouse_x_ = 128;
@@ -103,7 +105,8 @@ void loop() {
   Serial1.println(mouse_y);
 #endif
   touch.update();
-  if (touch.is_touched) {
+  if (touch.is_touched)
+  {
 #if debug
     Serial1.println("touch panel connected!!");
 #endif
@@ -116,75 +119,110 @@ void loop() {
     Serial1.print("mouse y : ");
     Serial1.println(mouse_y);
 #endif
-  } else {
+  }
+  else
+  {
 #if debug
     Serial1.println("no touch panel signal");
 #endif
   }
-  if (kb.need_reduce_fan_speed) {
-    if (io.fan_speed_value > io.fan_speed_min) {
+  if (kb.need_reduce_fan_speed)
+  {
+    if (io.fan_speed_value > io.fan_speed_min)
+    {
       io.fan_speed_value = io.fan_speed_value - io.fan_speed_step;
-    } else {
+    }
+    else
+    {
       io.fan_speed_value = 0;
     }
     io.fan_update(io.fan_speed_value);
     erom.write_fan_value(io.fan_speed_value);
     kb.need_reduce_fan_speed = false;
   }
-  if (kb.need_increase_fan_speed) {
+  if (kb.need_increase_fan_speed)
+  {
     io.fan_speed_value = io.fan_speed_value + io.fan_speed_step;
-    if (io.fan_speed_value > io.fan_speed_max) {
+    if (io.fan_speed_value > io.fan_speed_max)
+    {
       io.fan_speed_value = io.fan_speed_max;
     }
     io.fan_update(io.fan_speed_value);
     erom.write_fan_value(io.fan_speed_value);
     kb.need_increase_fan_speed = false;
   }
-  if (kb.need_reduce_backlight) {
-    if (io.backlight_value > io.backlight_min) {
+  if (kb.need_reduce_backlight)
+  {
+    if (io.backlight_value > io.backlight_min)
+    {
       io.backlight_value = io.backlight_value - io.backlight_step;
-    } else {
+    }
+    else
+    {
       io.backlight_value = 0;
     }
     io.backlight_update(io.backlight_value);
     erom.write_backlight_value(io.backlight_value);
     kb.need_reduce_backlight = false;
   }
-  if (kb.need_increase_backlight) {
-    if (io.backlight_value >= io.backlight_max) {
+  if (kb.need_increase_backlight)
+  {
+    if (io.backlight_value >= io.backlight_max)
+    {
       io.backlight_value = io.backlight_max;
-    } else {
+    }
+    else
+    {
       io.backlight_value = io.backlight_value + io.backlight_step;
     }
     io.backlight_update(io.backlight_value);
     erom.write_backlight_value(io.backlight_value);
     kb.need_increase_backlight = false;
   }
-  if (loop_count == 1) {
+  if (loop_count == 1)
+  {
     io.battery_voltage_update();
-  } else if (loop_count == 2) {
+  }
+  else if (loop_count == 2)
+  {
     io.update_charge_status();
-  } else if (loop_count == 3) {
+  }
+  else if (loop_count == 3)
+  {
     io.read_hdmi_status();
-  } else if (loop_count == 6) {
-    if (touch.is_touched) {
-  } else if (loop_count == 4) {
+  }
+  else if (loop_count == 4)
+  {
     ws.calculate(io.battery_percent, 0, kb.last_key, touch.is_touched, kb.capslock, erom.fan_pwm_value, erom.backlight_pwm_value);
-  } else if (loop_count == 5) {
+  }
+  else if (loop_count == 5)
+  {
     ws.update();
+  }
+  else if (loop_count == 6)
+  {
+    if (touch.is_touched)
+    {
       touch.is_touched--;
       iic_display.update(mouse_x, mouse_y, io.charge_or_not, io.charge_done_or_not, io.battery_percent, io.battery_voltage, io.screen_enabled, io.backlight_value, io.fan_speed_value, kb.keyboard_connect, kb.last_key);
-    } else if (kb.mouse_moved) {
+    }
+    else if (kb.mouse_moved)
+    {
       kb.mouse_moved = false;
       iic_display.update(mouse_x, mouse_y, io.charge_or_not, io.charge_done_or_not, io.battery_percent, io.battery_voltage, io.screen_enabled, io.backlight_value, io.fan_speed_value, kb.keyboard_connect, kb.last_key);
-    } else {
+    }
+    else
+    {
       iic_display.update(0, 0, io.charge_or_not, io.charge_done_or_not, io.battery_percent, io.battery_voltage, io.screen_enabled, io.backlight_value, io.fan_speed_value, kb.keyboard_connect, kb.last_key);
     }
-  } else if (loop_count == 7) {
+  }
+  else if (loop_count == 7)
+  {
     iic_display.flash();
   }
   loop_count++;
-  if (loop_count >= loop_count_max) {
+  if (loop_count >= loop_count_max)
+  {
     loop_count = 0;
   }
 #if debug
@@ -195,7 +233,8 @@ void loop() {
   Serial1.println((rp2040.getCycleCount64() - time_now) / 100000);
   Serial1.println("_____________________________________________________________________________________");
   // delay(300);
-  if (!digitalRead(gpio16)) {
+  if (!digitalRead(gpio16))
+  {
     if (allwaysdelay)
     {
       allwaysdelay = 0;
@@ -212,16 +251,20 @@ void loop() {
     Serial1.println(allwaysdelay);
     delay(1000);
   }
-  if (!digitalRead(gpio17)) {
+  if (!digitalRead(gpio17))
+  {
     delay(1000);
   }
-  if (!digitalRead(gpio18)) {
+  if (!digitalRead(gpio18))
+  {
     delay(3000);
   }
-  if (!digitalRead(gpio19)) {
+  if (!digitalRead(gpio19))
+  {
     delay(5000);
   }
-  if (!digitalRead(gpio20)) {
+  if (!digitalRead(gpio20))
+  {
     delay(8000);
   }
   delay(allwaysdelay + 1);
